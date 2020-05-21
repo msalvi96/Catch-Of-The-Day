cf8d05eba5fecd3a072cb90c13cd10fdcd60d38a
Mrunal <msalvi96@stevens.edu>
2020-05-21 01:07:56 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/styles.css';
 import App from './App';
+import NotFound from './components/NotFound';
+import StorePicker from './components/StorePicker';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Root = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={StorePicker} />
+          <Route path="/store/:storeId" component={App} />
+          <Route component={NotFound}/>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
